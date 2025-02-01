7f2350e271aeef8800a79f09824cc74890372c08
Yash <130866608+yasuyaswanth@users.noreply.github.com>
2025-02-01 11:50:37 +0530

    made debug=true

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def invalid_token_callback(error):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
